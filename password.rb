f5e7e1a096674c53085373f019e1565ce178a9f0
Danilo Martins <dansm86@outlook.com>
2024-06-12 15:56:19 -0300

    feat:validate password checking the password by: valid by size valid by uppercase valid by lower case valid by digit valid by symbol

---
+++ password.rb
@@ -0,0 +1,64 @@
+class Password
+  def initialize(conteudo)
+    @conteudo = conteudo
+    unless has_valid_size? and is_upper_case? and is_lower_case? and has_digit? and has_symbol?
+      raise 'Invalid Password'
+    end
+    puts 'Valid Password'
+    @power =
+  end
+
+  private
+
+  def has_valid_size?
+    if @conteudo.length > 9 and @conteudo.length < 31
+      return true
+    end
+
+    return false
+  end
+
+  def is_upper_case?
+    @conteudo.split('').each do |c|
+      if /[[:upper:]]/.match(c)
+        return true
+      end
+    end
+
+    return false
+  end
+
+  def is_lower_case?
+    @conteudo.split('').each do |c|
+      if /[[:lower:]]/.match(c)
+        return true
+      end
+    end
+
+    return false
+  end
+
+  def has_digit?
+    @conteudo.split('').each do |c|
+      if /\d/.match(c)
+        return true
+      end
+    end
+
+    return false
+  end
+
+  def has_symbol?
+    @conteudo.split('').each do |c|
+      if /\W/.match(c)
+        return true
+      end
+    end
+
+    return false
+  end
+
+end
+
+
+password = Password.new('danil*QWwewe')
